e56dcd773401e6cb651193e3f61709271977bcda
warit <warit.boonmasiri@gmail.com>
2020-06-04 01:04:07 +0700

    Initial commit

---
+++ src/main/java/com/klur/stock/stocklinebotnotifier/StockLinebotNotifierApplication.java
@@ -0,0 +1,13 @@
+package com.klur.stock.stocklinebotnotifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockLinebotNotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockLinebotNotifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/klur/stock/stocklinebotnotifier/StockLinebotNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klur.stock.stocklinebotnotifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockLinebotNotifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
